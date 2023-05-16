a96f923d586ae99647bd92de623bc7381cb132bb
pawel <12714318+pawelbudnik@users.noryply.github.com>
2023-05-16 18:48:00 +0200

    basic functionality

--- scripts/content.js
+++ scripts/content.js
@@ -1,73 +1,122 @@
-
-// Make those into an object maybe and create dynamically with custom names of bindButton1... 2... 3 etc.
-let keybind = null; // Variable to store the keybind
-let bindButton = document.getElementById('bindButton');
-let clearButton = document.getElementById('clearButton');
-let addButton = document.getElementById('addButton');
-let videoInput = document.getElementById('videoInput');
+let buttonsArray = [];
+const container = document.getElementById('buttonContainer');
+const bindButton = document.getElementById('bindButton');
+const addButton = document.getElementById('addButton');
+const videoInput = document.getElementById('videoInput');
+let keybind = null;
 let isListening = false;
 
-// Regex function to extract video id from the url
-function extractVideoId(url) {
-  // Regular expression to match the video ID in the URL
-  const regExp = /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?.*v=([a-zA-Z0-9_-]{11}).*/;
-
-  // Extract the video ID using the regular expression
-  const match = url.match(regExp);
-
-  if (match && match.length === 2) {
-    return match[1]; // Return the extracted video ID
-  } else {
-    return null; // Return null if the video ID could not be extracted
-  }
+// Function to initialize the extension
+function initializeExtension() {
+  loadButtonsFromStorage();
+  bindEventListeners();
 }
 
-// Function to handle the keydown event
-function handleKeyDown(event) {
-  event.preventDefault(); // Prevent any default actions of the key event
-  
-  const key = event.key; // Get the key that was pressed
-  keybind = key; // Store the keybind in the variable
-  console.log('Keybind set:', keybind); // Display the keybind in the console or perform any desired action
-
-  // You can assign the keybind to a button's text, for example:
-  bindButton.innerText = 'Keybind = ' + keybind;
-  document.removeEventListener('keydown', handleKeyDown);
-  isListening = false;
-}
-
-// Function to handle the button click event
-function handleButtonClick() {
-  if (!isListening) {
-    bindButton.innerText = "listening for the key"
-    document.addEventListener('keydown', handleKeyDown); // Listen for the keydown event
-    console.log('Set the keybind now:');
-    isListening = true;
-  }
-}
-
-// Function to handle the clear button click event
-function handleClearButtonClick() {
-  keybind = null; // Clear the stored keybind
-  
-  // You can clear the button's text, for example:
-  bindButton.innerText = 'Click to Set Keybind';
-}
-
-function handleAddButtonClick() {
-  // let videoId = "GNJtPFXUnm4";
-  let videoId = extractVideoId(videoInput.value);
-  
+// Function to handle the play button click event
+function handlePlayButtonClick(buttonObject) {
   const iframe = document.createElement('iframe');
-  iframe.src = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1';
+  iframe.src = `https://www.youtube.com/embed/${buttonObject.videoId}?autoplay=1`;
   iframe.style.display = 'none';
-
-  console.log("Source: ", iframe.src);
-
   document.body.appendChild(iframe);
 }
 
-// Attach event listeners to the buttons
-bindButton.addEventListener('click', handleButtonClick);
-clearButton.addEventListener('click', handleClearButtonClick);
-addButton.addEventListener('click', handleAddButtonClick);
+// Function to render the buttons
+function renderButtons() {
+  container.innerHTML = '';
+  container.classList.add('button-container');
+
+  buttonsArray.forEach(function(buttonObject, index) {
+    const circleButton = document.createElement('button');
+    circleButton.classList.add('circle-button');
+
+    const keybindText = document.createElement('span');
+    keybindText.innerText = buttonObject.keybind ? `(${buttonObject.keybind})` : '';
+    circleButton.appendChild(keybindText);
+
+    const soundNameText = document.createElement('span');
+    soundNameText.innerText = buttonObject.soundName;
+    soundNameText.classList.add('sound-name');
+    circleButton.appendChild(soundNameText);
+
+    circleButton.addEventListener('click', function() {
+      handlePlayButtonClick(buttonObject);
+    });
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Remove';
+    removeButton.classList.add('remove-button');
+    removeButton.addEventListener('click', function() {
+      handleRemoveButtonClick(index);
+    });
+
+    circleButton.appendChild(removeButton);
+    container.appendChild(circleButton);
+  });
+}
+
+function handleRemoveButtonClick(index) {
+  buttonsArray.splice(index, 1);
+  chrome.storage.local.set({ buttonsArray: buttonsArray });
+  renderButtons();
+}
+
+// Function to load buttons from storage
+function loadButtonsFromStorage() {
+  chrome.storage.local.get('buttonsArray', function(result) {
+    if (result.buttonsArray) {
+      buttonsArray = result.buttonsArray;
+      renderButtons();
+    }
+  });
+}
+
+// Function to bind event listeners
+function bindEventListeners() {
+  // Regex function to extract video ID from the URL
+  function extractVideoId(url) {
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?.*v=([a-zA-Z0-9_-]{11}).*/;
+    const match = url.match(regExp);
+    return match && match.length === 2 ? match[1] : null;
+  }
+
+  // Function to handle the keydown event
+  function handleKeyDown(event) {
+    event.preventDefault();
+    keybind = event.key;
+    bindButton.innerText = `Bound to "${keybind}"`;
+    document.removeEventListener('keydown', handleKeyDown);
+    isListening = false;
+  }
+
+  // Function to handle the button click event
+  function handleBindClick() {
+    if (!isListening) {
+      bindButton.innerText = 'Listening for the key';
+      document.addEventListener('keydown', handleKeyDown);
+      isListening = true;
+    }
+  }
+
+  // Function to handle the add button click event
+  function handleAddButtonClick() {
+    const soundName = document.getElementById('soundName').value;
+    const videoId = extractVideoId(videoInput.value);
+
+    const buttonObject = {
+      soundName: soundName,
+      keybind: keybind,
+      videoId: videoId
+    };
+
+    buttonsArray.push(buttonObject);
+    chrome.storage.local.set({ buttonsArray: buttonsArray });
+    renderButtons();
+  }
+
+  // Attach event listeners to the buttons
+  bindButton.addEventListener('click', handleBindClick);
+  addButton.addEventListener('click', handleAddButtonClick);
+}
+
+// Initialize the extension
+initializeExtension();
